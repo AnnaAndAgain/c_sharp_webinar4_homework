a7056a20b848e71755fde0cbbfa0195497a4f3fe
Anna <a.s.markovskaya@gmail.com>
2023-07-02 11:12:14 +0300

    черновые решения

---
+++ task-25/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+Console.Write("Введите число:");
+double A = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите степень (натуральное число):");
+int B = Convert.ToInt32(Console.ReadLine());
+
+if (B>0) 
+{
+    double result = 1;
+    for (int i = 1; i <= B; i++)
+    {
+        result = result * A;
+    }
+    System.Console.WriteLine(result);
+}
+else System.Console.WriteLine("Введите целую положительную степень!");
---
+++ task-27/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число 
+и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/ 
+
+
+/// тут ваще мимо
+
+Console.Write("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int[] digits;
+
+for (int i = 1; i < N*10; i++)
+{
+    int 
+
+}
+
+Console.WriteLine(sum);
